2cc1e4c154da8dbb6543a24e87d24f9211908a83
Ricardo Rodriguez <ricarod@ricardos-macbook-pro.local>
2016-01-14 06:57:00 -0500

    Initial Commit

---
+++ TFcontacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TFcontacts
+//
+//  Created by Ricardo Rodriguez on 1/14/16.
+//  Copyright © 2016 Ricardo Rodriguez P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
